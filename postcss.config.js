e251a6e2c1cd6a5aa733bbcf381401d84391c3f1
Harikrishnan K P <54279872+Harikrishnan009@users.noreply.github.com>
2026-07-27 19:02:37 +0530

    Restore PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
-import NavBar from "@/components/NavBar";
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Experience from "@/components/Experience";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import Certifications from "@/components/Certifications";
-import Contact from "@/components/Contact";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-bg">
-      <NavBar />
-      <Hero />
-      <About />
-      <Experience />
-      <Skills />
-      <Projects />
-      <Certifications />
-      <Contact />
-    </main>
-  );
-}
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
